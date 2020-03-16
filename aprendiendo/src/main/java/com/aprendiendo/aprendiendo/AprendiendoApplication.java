b21d3b2665bc18c97c5202bf52b0398963b2b7ce
jhonny perez <jhonnyperezmo@gmail.com>
2020-03-15 23:39:29 -0500

    Primer commit

---
+++ aprendiendo/src/main/java/com/aprendiendo/aprendiendo/AprendiendoApplication.java
@@ -0,0 +1,13 @@
+package com.aprendiendo.aprendiendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendiendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendiendoApplication.class, args);
+	}
+
+}
